865f04ad2bd8fcf42f2af0153ba7bfc270f26b64
zb@dot-dot.ru <zb@dot-dot.ru>
2021-11-20 23:27:02 +0300

    base styles added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+  res.render(path.join(__dirname, 'views', 'main-page'))
+});
+
+app.listen(3000);
